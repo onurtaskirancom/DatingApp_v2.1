58536dfb0258f0d8e12265e1e04c7a073e582150
onurtaskirancom <taskiranonur9@gmail.com>
2021-03-19 08:31:23 +0300

    End of section 16

--- API/Extensions/IdentityServiceExtensions.cs
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -1,5 +1,8 @@
 using System.Text;
+using API.Data;
+using API.Entities;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
@@ -11,16 +14,32 @@ namespace API.Extensions
         public static IServiceCollection AddIdentityServices(this IServiceCollection services, 
             IConfiguration config)
             {
-              services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-                .AddJwtBearer(options => 
+                services.AddIdentityCore<AppUser>(opt => 
                 {
-                    options.TokenValidationParameters = new TokenValidationParameters
+                    opt.Password.RequireNonAlphanumeric = false;
+                })
+                    .AddRoles<AppRole>()
+                    .AddRoleManager<RoleManager<AppRole>>()
+                    .AddSignInManager<SignInManager<AppUser>>()
+                    .AddRoleValidator<RoleValidator<AppRole>>()
+                    .AddEntityFrameworkStores<DataContext>();
+
+              services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                    .AddJwtBearer(options => 
                     {
-                        ValidateIssuerSigningKey = true,
-                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"])),
-                        ValidateIssuer = false,
-                        ValidateAudience = false,
-                    };
+                        options.TokenValidationParameters = new TokenValidationParameters
+                        {
+                            ValidateIssuerSigningKey = true,
+                            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"])),
+                            ValidateIssuer = false,
+                            ValidateAudience = false,
+                        };
+                    });
+
+                services.AddAuthorization(opt => 
+                {
+                    opt.AddPolicy("RequireAdminRole", policy => policy.RequireRole("Admin"));
+                    opt.AddPolicy("ModeratePhotoRole", policy => policy.RequireRole("Admin", "Moderator"));
                 });
 
                 return services;
